b428ef5cec5b39ef2b7fc4d9250a4332ff1b864e
SravaniMalempati <malempatis93@gmail.com>
2018-05-30 20:21:07 -0500

    Second commit

--- XyzException.java
+++ XyzException.java
@@ -1,6 +1,9 @@
 public class XyzException extends Exception
 {
-
+if (age<18)
+	throw new XyzException("Minors not allowed");
+else if(age>120)
+	throw new XyzException("invalid age");
 XyzException(String msg)
 {
 	super(msg);
